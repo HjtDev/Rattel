743191f5aa20d7a298fc31ee7e6bed4938bb0dea
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2026-05-19 17:25:20 +0330

    fix(frontend): Fix index page SEO
    - Fixed index page didn't have any SEO tags

--- rattel-frontend/app/page.tsx
+++ rattel-frontend/app/page.tsx
@@ -5,9 +5,6 @@ export const metadata: Metadata = {
   title: "خانه",
   description:
     "اکسیر قرآن؛ سامانه آموزش جامع قرآن با دوره های حفظ، روخوانی، تجوید، مسابقات قرآنی و مسیر پیشرفت آموزشی.",
-  alternates: {
-    canonical: "/",
-  },
 };
 
 export default function Home() {
